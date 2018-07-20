ed77bf6f331806a8ad07f5ff5dd64006ed8d9f9d
JS <lamjs-wa15@student.tarc.edu.my>
2018-07-20 20:53:09 +0800

    v1

--- app/src/main/java/my/edu/tarc/hciprototype/MainActivity.java
+++ app/src/main/java/my/edu/tarc/hciprototype/MainActivity.java
@@ -2,12 +2,18 @@ package my.edu.tarc.hciprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText2 = (EditText)findViewById(R.id.price2);
+        editText2.setText("hello");
+
+
     }
 }
